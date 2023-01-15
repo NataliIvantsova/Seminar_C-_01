bd55685c4a4d4b946cad5d2fde3e4fbd5420ea88
NataliIvantsova <ivancovanm@yandex.ru>
2023-01-15 11:27:15 +0300

    Add Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+// 1 -> 1 
+// 2 -> 0.5
+// 0.25 -> 1/4
+
+System.Console.WriteLine("введите число > ");
+
+double numer = Convert.ToInt32(Console.ReadLine());
+double resalt = 1/numer;
+
+System.Console.WriteLine($"{resalt}");
+
