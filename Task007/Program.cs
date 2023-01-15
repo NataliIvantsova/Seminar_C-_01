98f2cfc022e148fff9151c816bc03deae852d71e
NataliIvantsova <ivancovanm@yandex.ru>
2023-01-15 11:46:12 +0300

    git add Task00

---
+++ Task007/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу вычисления модуля числа.
+// 2 -> 2
+// -3 -> 3
+// -7 -> 7
+
+System.Console.WriteLine("введите число > ");
+
+double numer = Convert.ToInt32(Console.ReadLine());
+if(numer>=0)
+{
+    System.Console.WriteLine($"модулем числа {numer} является квадратом числа {numer}");
+}
+else
+{
+        System.Console.WriteLine($"число {numer} не  является квадратом числа {numer/-1}");
+}
+
